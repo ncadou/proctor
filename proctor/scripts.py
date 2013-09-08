46d8a2dcda4231ab25b74c72bb28e136bdda0a08
Nicolas Cadou <ncadou@cadou.ca>
2013-09-08 12:14:44 -0400

    added an option to configure the proxy listening port

--- proctor/scripts.py
+++ proctor/scripts.py
@@ -22,6 +22,8 @@ log = logging.getLogger(__name__)
 def parse_args():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('-d', '--work-dir', help='Working directory')
+    parser.add_argument('-p', '--port', type=int, default=8080,
+                        help='Proxy server listening port')
     parser.add_argument('-s', '--base-socks-port', type=int, default=9051,
                         help='Base socks port for the Tor processes')
     parser.add_argument('-c', '--base-control-port', type=int, default=8119,
@@ -34,19 +36,23 @@ def parse_args():
 def main():
     args = parse_args()
     work_dir = args.work_dir or mkdtemp()
+    proxy = None
     try:
         tor_swarm = TorSwarm(args.base_socks_port, args.base_control_port,
                              work_dir)
         tor_instances = tor_swarm.start(args.instances)
-        log.info('Waiting for one connected Tor instance...')
+        log.debug('Waiting for at least one connected Tor instance...')
         while not [t for t in tor_instances if t.connected]:
             sleep(0.25)
         handler_factory = tor_proxy_handler_factory(tor_swarm)
-        proxy = AsyncMitmProxy(RequestHandlerClass=handler_factory)
+        proxy = AsyncMitmProxy(server_address=('', args.port),
+                               RequestHandlerClass=handler_factory)
+        log.info('Starting proxy server on port %s' % args.port)
         proxy.serve_forever()
     except KeyboardInterrupt:
-        print '\nCtrl C - Stopping server'
-        proxy.server_close()
+        if proxy:
+            print '\nCtrl C - Stopping server'
+            proxy.server_close()
         sys.exit(1)
     finally:
         tor_swarm.stop()
